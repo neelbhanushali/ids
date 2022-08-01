b843fa6bc8ea49f9726dfa5129760a3723c8319b
Neel Bhanushali <neal.bhanushali@gmail.com>
2022-08-01 18:50:59 +0530

    identity marriage done

---
+++ IdentityServer/Integration/EmailSender.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Integration
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // do email integration here
+           return Task.CompletedTask;
+        }
+    }
+}
